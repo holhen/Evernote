438f13c5bc5135ced891637568968bca6fc15e8b
holhen <holhen@gmail.com>
2019-10-04 10:20:38 +0200

    Updates

---
+++ precache-manifest.4aa0eea24f89b290215e9dd8fd13ba4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff2f63b8e5ee0264dcaa4e7899b64fd",
+    "url": "/Evernote/index.html"
+  },
+  {
+    "revision": "5ff3e5936e8be7d3611a",
+    "url": "/Evernote/static/css/main.65553a91.chunk.css"
+  },
+  {
+    "revision": "9a9ac93b5ed1582b1026",
+    "url": "/Evernote/static/js/2.55ef5313.chunk.js"
+  },
+  {
+    "revision": "5ff3e5936e8be7d3611a",
+    "url": "/Evernote/static/js/main.670a9511.chunk.js"
+  },
+  {
+    "revision": "86399eaf14088a92c836",
+    "url": "/Evernote/static/js/runtime~main.e6c878c4.js"
+  }
+]);
